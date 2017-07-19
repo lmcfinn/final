eb6e01180b7fbd8febf14e53b16ba05eb00fa330
Shane <lmcfinn@yahoo.com>
2017-07-19 14:04:52 -0500

    wtf

--- app/components/children/Signup.js
+++ app/components/children/Signup.js
@@ -12,7 +12,11 @@ var Signup = React.createClass({
   },
 
   setSignUp: function(email, password) {
-    helpers.signUp().then(function(data) {
+
+    console.log("setSignup email", email)
+    console.log("setSignup password", password)
+
+    helpers.signUp(email, password).then(function(data) {
       console.log("getSignUp data", data)
       this.setState({
         email: data.email,
@@ -21,20 +25,49 @@ var Signup = React.createClass({
     }.bind(this));
   },
 
+  // setEmail: function(email) {
+  //   this.setState({ email: email });
+  // },
+
+  // setPassword: function(password) {
+  //   this.props.setState({password: password});
+  // },
+  
+  handleChange(data) {
+    console.log("handleChangeEmail", data.target.value)
+    this.setState({email: data.target.value});
+  },
+
+  // handleChange(password) {
+  //   console.log("handleChangePassword", password.target.value)
+  //   this.setState({password: password.target.value});
+  // },
 
 
-  handleClick: function (event) {
+
+  // handleClick: function (event) {
+  //       event.preventDefault();
+  //       console.log("CLICKED");
+  //       // this.props.updateSearch(this.state.search);
+  //       this.setSignUp();
+  // },
+
+  handleSubmit: function (event) {
         event.preventDefault();
-        console.log("CLICKED");
+        console.log("Submitted");
+        
+        // var email = {this.state.value}
+        // console.log("handleSubmit", email)
+
         // this.props.updateSearch(this.state.search);
-        var tenK = this.setSignUp();
+        this.setSignUp();
   },
 
 	render: function() {
 		return (
 
 			<div className="container">
-				<form id="signup" name="signup">
+				<form id="signup" name="signup" onSubmit={this.handleSubmit} >
                 <div className="row">
                     <h2>Sign Up</h2>
                     <h2 id="invalidSignup"></h2>
@@ -42,14 +75,14 @@ var Signup = React.createClass({
                 </div>
                 <div className="row">
                   <label>Email Address *</label>
-                  <input className="text" name="email" type="email"/>
+                  <input className="text" name="email" type="email" value={this.state.value} onChange={this.handleChange} />
                 </div>
                 <div className="row">
                     <label>Password *</label>
-                    <input name="password" type="password" className="text"/>
+                    <input name="password" type="password" className="text" />
                 </div>
                 <div className="row">
-                  <button id="signupButton" className="btn" onClick={this.handleClick} >Submit</button>
+                  <button id="signupButton" className="btn" type="submit" >Submit</button>
                 </div>
               </form>
               {this.props.children}
